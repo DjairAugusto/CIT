1e25aee20f677e1215318e92e44a2e2810f835a3
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-10-06 03:11:35 -0300

    Configuração do tailwind

--- src/front/tailwind.config.js
+++ src/front/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#42D0B3",
+				secondary: "#F6F8F7",
+			},
+			fontFamily: {
+				sans: ["Work Sans", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
+};
