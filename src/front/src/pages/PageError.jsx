597156247e6ca11c2202bc29557bc48df6370a6b
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-10-06 03:15:56 -0300

    Adição das paginas de exemplo

---
+++ src/front/src/pages/PageError.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link, useRouteError } from "react-router-dom";
+
+export default function PageError() {
+	const error = useRouteError();
+	console.error(error);
+	return (
+		<>
+			<h1>Oops!</h1>
+			<p>desnculpa, ocorreu um erro inesperado</p>
+			<p>
+				<i>{error.statusText || error.message}</i>
+			</p>
+			<Link to="/">Voltar para a página inicial</Link>
+		</>
+	);
+}
