620cb2ea36c0c4ae1aeacacbe5f08ba0c41aab59
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-10-06 03:16:46 -0300

    Arquivo inicial

--- src/front/src/index.js
+++ src/front/src/index.js
@@ -1,14 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Main from "./routers/RootMain";
+import Register from "./routers/RootRegister";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
+import Projects from "./pages/Projects";
+import Services from "./pages/Services";
+import PageError from "./pages/PageError";
+import CondominiumRegister from "./pages/CondominiumRegister";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Main />,
+		errorElement: <PageError />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/about",
+				element: <About />,
+			},
+			{
+				path: "/projects",
+				element: <Projects />,
+			},
+			{
+				path: "/services",
+				element: <Services />,
+			},
+			{
+				path: "/contacts",
+				element: <Contacts />,
+			},
+		],
+	},
+	{
+		path: "/register",
+		element: <Register />,
+		errorElement: <PageError />,
+		children: [
+			{
+				path: "/register/condominium",
+				element: <CondominiumRegister />,
+			},
+		],
+	},
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
