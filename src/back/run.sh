cf6a986485e3939461d5f2b0cf08707f63392ce3
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-10-06 21:48:10 -0300

    Correção do doocker

--- src/back/run.sh
+++ src/back/run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-/app/mvnw spring-boot:run &
-
-while true; do
-	inotifywait -e modify,create,delete,move -r ./src/ && /app/mvnw  compile
-done
+dos2unix ./mvnw
+./mvnw spring-boot:run
