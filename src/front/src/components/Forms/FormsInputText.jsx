34fe05faee7e56439129b657c1d2ccb0ecf584d6
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-10-07 03:55:45 -0300

    Adição de funcionalidades do inputText

--- src/front/src/components/Forms/FormsInputText.jsx
+++ src/front/src/components/Forms/FormsInputText.jsx
@@ -2,7 +2,10 @@ import { EyeOff } from "lucide-react";
 import React, { forwardRef, useState } from "react";
 
 const Input = forwardRef(
-	({ LeftIcon, RightIcon, type, required, placeholder, ...rest }, ref) => {
+	(
+		{ LeftIcon, RightIcon, type, required, placeholder, id, ...rest },
+		ref
+	) => {
 		const [useType, setUseType] = useState(type);
 
 		function seePassword() {
@@ -18,23 +21,27 @@ const Input = forwardRef(
 				/>
 			);
 
-		if (required) placeholder += " *";
-
 		return (
-			<div
-				className="flex items-center gap-4 px-4 border-sm  border-medium bg-gray-100 
-    dark:border-light dark:bg-dark "
-			>
+			<label className="flex items-center gap-4 px-4 py-2 border-sm border-medium bg-zinc-50 border-b-2 border-transparent has-[:focus]:border-primary-1000 transition-colors duration-300">
 				{LeftIcon ? LeftIcon : ""}
-				<input
-					className="bg-transparent h-12 w-full outline-none text-dark placeholder:text-dark text-zinc-700 placeholder:text-zinc-700"
-					placeholder={placeholder}
-					type={useType}
-					{...rest}
-					ref={ref}
-				/>
+				<div className="relative w-full">
+					<input
+						className="bg-transparent h-12 w-full outline-none text-dark placeholder:text-dark text-zinc-700 placeholder:text-zinc-700 peer transition-colors pl-4"
+						type={useType}
+						{...rest}
+						ref={ref}
+					/>
+					<label className="text-zinc-600 absolute cursor-text left-0 top-1/4 peer-focus:top-0 peer-focus:text-xs [&:not(peer-empty:bg-black)] transition-colors duration-300">
+						{required ? (
+							<span className="text-red-500">*</span>
+						) : (
+							""
+						)}
+						{placeholder}
+					</label>
+				</div>
 				{RightIcon ? RightIcon : ""}
-			</div>
+			</label>
 		);
 	}
 );
