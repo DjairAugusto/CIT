78e0292816c5ca58198332633ac204eb1d1eedef
jaoppb <joaopedroperes06@gmail.com>
2024-10-07 16:38:15 -0300

    chevron arrows corrigidas
    TODO: adicionar animação

--- src/front/src/components/DropMenu.jsx
+++ src/front/src/components/DropMenu.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, ChevronUp, CircleMinus } from "lucide-react";
 
-export default function DropMenu({ text, children }) {
+export default function DropMenu({ text, children, removeCallback }) {
 	if (!text) text = "Selecione uma opção";
 	const [isChecked, setIsChecked] = useState(false);
 	const handleCheckboxChange = (event) => {
@@ -17,7 +17,10 @@ export default function DropMenu({ text, children }) {
 					onChange={handleCheckboxChange}
 				/>
 				{text}
-				<ChevronDown />
+				<div className="flex gap-4">
+					{removeCallback && (<CircleMinus onClick={removeCallback} />)}
+					{isChecked ? <ChevronUp /> : <ChevronDown />}
+				</div>
 			</label>
 			<div className={isChecked ? "p-4" : "hidden"}>
 				{isChecked ? children : ""}
