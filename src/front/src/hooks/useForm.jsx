fe7b3ae4cb9267bd5e7d239ec373d215dbf602b3
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-10-07 12:57:57 -0300

    Adição do hook useForm

---
+++ src/front/src/hooks/useForm.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function useForm(steps) {
+	const [currentStep, setCurrentStep] = useState(0);
+
+	function changeSteps(i, e) {
+		if (e) e.preventDefault();
+		if (i < 0 || i >= steps.length) return;
+
+		setCurrentStep(i);
+	}
+
+	return {
+		currentStep,
+		currentComponent: steps[currentStep],
+		isLast: currentStep + 1 === steps.length ? true : false,
+		isFist: currentStep === 0 ? true : false,
+		changeSteps,
+	};
+}
