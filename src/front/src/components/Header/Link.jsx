f59e28bb3865bd7c99ddd1e7da989dff130c8233
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-10-07 04:00:07 -0300

    Correção do link do header

--- src/front/src/components/Header/Link.jsx
+++ src/front/src/components/Header/Link.jsx
@@ -4,7 +4,7 @@ import { Link as LinkRouter } from "react-router-dom";
 export default function Link(props) {
 	return (
 		<LinkRouter
-			className="hover:text-primary hover:border-black border-transparent border-2 text-black px-5 py-2 transition-all duration-300"
+			className="hover:text-primary-1000 hover:border-primary-1000 font-semibold border-transparent border-2 text-black px-5 py-2 transition-all duration-300"
 			{...props}
 		>
 			{props.children}
