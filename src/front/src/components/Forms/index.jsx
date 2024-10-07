cf3c3e29b2b46d2081c2214e40a13c6c531d6880
jaoppb <joaopedroperes06@gmail.com>
2024-10-07 17:00:56 -0300

    adição do select ao component forms
    TODO: refatorar sem o select e o option

--- src/front/src/components/Forms/index.jsx
+++ src/front/src/components/Forms/index.jsx
@@ -1,9 +1,11 @@
 import FormsButton from "./FormsButton";
 import FormsInputText from "./FormsInputText";
 import FormsRoot from "./FormsRoot";
+import FormsSelect from "./FormsSelect";
 
 export const Forms = {
 	Root: FormsRoot,
 	InputText: FormsInputText,
 	Button: FormsButton,
+	Select: FormsSelect
 };
