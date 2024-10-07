5671f54b1978a03a4e22025a99fc80e623b67a88
jaoppb <joaopedroperes06@gmail.com>
2024-10-07 16:39:01 -0300

    código inicial da página de cadastro de apartamento

--- src/front/src/pages/ApartmentRegistration/index.jsx
+++ src/front/src/pages/ApartmentRegistration/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ApartmentForm from "./ApartmentForm";
+import InhabitantForm from "./InhabitantForm";
 import VehicleForm from "./VehicleForm";
 import ProfileForm from "./ProfileForm";
 import useForm from "../../hooks/useForm";
@@ -7,7 +7,7 @@ import { Forms } from "../../components/Forms";
 import { ArrowLeft } from "lucide-react";
 
 export default function ApartmentRegistration() {
-	const steps = [<ApartmentForm />, <VehicleForm />, <ProfileForm />];
+	const steps = [<InhabitantForm />, <VehicleForm />, <ProfileForm />];
 	const { currentStep, currentComponent, changeSteps, isLast, isFist } =
 		useForm(steps);
 
@@ -30,7 +30,7 @@ export default function ApartmentRegistration() {
 			</div>
 			<div
 				onSubmit={(e) => changeSteps(currentStep + 1, e)}
-				className="w-full flex flex-col justify-between items-center pb-24 m-auto px-4 h-full relative lg:px-20 lg:py-12 sm:w-[600px]"
+				className="w-full flex flex-col justify-between items-center pb-24 py-8 m-auto px-4 h-full relative lg:px-16 lg:py-12 sm:w-[600px]"
 			>
 				{currentComponent}
 				<div className="flex justify-evenly w-full">
