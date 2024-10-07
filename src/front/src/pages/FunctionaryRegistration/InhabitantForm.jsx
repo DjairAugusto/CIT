e0f353aceabec24504f0d127eab2b4087c228529
Djair Augusto <dagsoares@sga.pucminas.br>
2024-10-07 20:43:59 -0300

    Versão 3 cadastro de condomínio

---
+++ src/front/src/pages/FunctionaryRegistration/InhabitantForm.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import DropMenu from "../../components/DropMenu";
+import { Forms } from "../../components/Forms";
+
+let nextId = 0;
+
+export default function InhabitantForm() {
+	const [inhabitants, setInhabitant] = useState([
+		{
+			id: nextId++,
+			name: "",
+			cpf: "",
+			phone: "",
+			email: "",
+		}
+	]);
+
+	function addInhabitant() {
+		setInhabitant([
+			...inhabitants,
+			{
+				id: nextId++,
+				name: "",
+				cpf: "",
+				phone: "",
+				email: "",
+			}
+		]);
+	}
+
+	function removeInhabitant(target) {
+		setInhabitant(inhabitants.filter(value => value.id !== target));
+	}
+
+	function modifyInhabitant(target, [key, value]) {
+		const newInhabitants = [];
+		Object.assign(newInhabitants, inhabitants);
+		const inhabitant = newInhabitants.find(value => value.id === target);
+		inhabitant[key] = value;
+		
+		setInhabitant(newInhabitants);
+	}
+
+	return (
+		<div className="flex flex-col gap-6 justify-between items-center w-full h-fit">
+			<div className="w-full">
+				<h1 className="text-center text-4xl mb-4">
+					Cadastro de Funcionário
+				</h1>
+				<div className="w-full flex items-center justify-center">
+					<div className="bg-zinc-400 h-[1px] w-full"></div>
+					<h6 className="px-4 whitespace-nowrap m-auto">
+						Funcionário XXXX
+					</h6>
+					<div className="bg-zinc-400 h-[1px] w-full"></div>
+				</div>
+			</div>
+
+			<div className="w-full h-fit">
+				<button
+					className="text-primary-1000 block mb-2"
+					onClick={addInhabitant}
+				>+ Adicionar Funcionário</button>
+				<div className="flex flex-col gap-6 overflow-y-auto max-h-[600px]">
+					{
+						inhabitants.map(inhabitant => (
+							<DropMenu
+								key={inhabitant.id}
+								text={inhabitant.name ? inhabitant.name : "Nome do Funcionário"}
+								removeCallback={inhabitants.indexOf(inhabitant) === 0 ? null : () => removeInhabitant(inhabitant.id)}
+							>
+								<div className="flex gap-4 flex-col">
+									<Forms.InputText
+										placeholder="NOME DO FUNCIONÁRIO"
+										onChange={e => modifyInhabitant(inhabitant.id, ["name", e.nativeEvent.srcElement.value])}
+										required
+									/>
+									<Forms.InputText
+										placeholder="CARGO"
+										onChange={e => modifyInhabitant(inhabitant.id, ["cargo", e.nativeEvent.srcElement.value])}
+										required
+									/>
+
+									<Forms.InputText
+										placeholder="TELEFONE"
+										onChange={e => modifyInhabitant(inhabitant.id, ["phone", e.nativeEvent.srcElement.value])}
+									/>
+
+									<Forms.InputText
+										placeholder="E-MAIL"
+										onChange={e => modifyInhabitant(inhabitant.id, ["email", e.nativeEvent.srcElement.value])}
+									/>
+								</div>
+							</DropMenu>
+						))
+					}
+				</div>
+			</div>
+		</div>
+	);
+}
