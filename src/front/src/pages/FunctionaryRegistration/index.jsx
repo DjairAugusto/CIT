6d46070fe43d18111a96c329c64014afabcabb61
Djair Augusto <166445437+DjairAugusto@users.noreply.github.com>
2024-10-07 17:00:05 -0300

    Primeira versão cadastro de funcionários

---
+++ src/front/src/pages/FunctionaryRegistration/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import FunctionaryForm from "./FunctionaryForm";
+import useForm from "../../hooks/useForm";
+
+export default function FunctionaryRegistration() {
+    return(
+        <FunctionaryForm/>
+    );
+}
