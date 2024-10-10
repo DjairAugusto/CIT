77e430d97bbc5ef835064237ce626b4e613def3c
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-10-10 04:01:03 -0300

    Criação da classe de exemplo

--- src/back/src/main/java/com/cit/backend/controller/HelloWorldController.java
+++ src/back/src/main/java/com/cit/backend/controller/HelloWorldController.java
@@ -3,12 +3,21 @@ package com.cit.backend.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.cit.backend.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/")
 public class HelloWorldController {
-    @GetMapping
-    public String helloWorld() {
-        return "Hello, World!";
+
+    @Autowired
+    private HelloWorldService helloWorldService;
+
+    @GetMapping("/")
+    public String helloWorld(@PathVariable("name") String name) {
+        // pegar os parametros da requisição
+        return "helloWorldService.helloWorld(name)";
     }
 }
