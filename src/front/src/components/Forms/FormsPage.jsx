44b487f0e16a7f49c8b2a4db7a74ad3d1d96878a
jaoppb <joaopedroperes06@gmail.com>
2024-10-12 10:35:15 -0300

    implementação do componente

---
+++ src/front/src/components/Forms/FormsPage.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Forms } from ".";
+import { ArrowLeft } from "lucide-react";
+import useForm from "../../hooks/useForm";
+
+export default function FormsPage({steps, imageSource}) {
+	const { currentStep, currentComponent, changeSteps, isLast, isFirst } = useForm(steps);
+	
+	return (
+		<main className="flex justify-between h-svh bg-zinc-100">
+			<div className="absolute left-5 z-10">
+				{!isFirst && (
+					<Forms.Button
+						className="w-24 flex items-center gap-1 justify-center"
+						type="button"
+						onClick={() => changeSteps(currentStep - 1)}
+					>
+						<ArrowLeft />
+						Voltar
+					</Forms.Button>
+				)}
+			</div>
+			<div
+				onSubmit={(e) => changeSteps(currentStep + 1, e)}
+				className="w-full flex flex-col justify-between items-center pb-24 py-8 m-auto px-4 h-full relative lg:px-16 lg:py-12 sm:w-[600px]"
+			>
+				{currentComponent}
+				<div className="flex justify-evenly w-full">
+					{
+						isLast ? (
+							<Forms.Button
+								className="w-2/5"
+								type="submit"
+							>
+								Cadastrar
+							</Forms.Button>
+						) : (
+							<Forms.Button
+								className="w-2/5"
+								type="button"
+								onClick={(e) => changeSteps(currentStep + 1, e)}
+							>
+								Próximo
+							</Forms.Button>
+						)
+					}
+				</div>
+			</div>
+			<img
+				className="hidden lg:block w-1/2 object-cover"
+				src={imageSource}
+				alt=""
+			/>
+		</main>
+	);
+}
