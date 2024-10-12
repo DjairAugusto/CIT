1481a94754d0f981bfbc6c279708a5707f852ff4
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-10-12 05:32:07 -0300

    Adição do exemplo de codigo

---
+++ src/back/src/main/java/com/cit/backend/domain/service/CondominiumService.java
@@ -0,0 +1,31 @@
+package com.cit.backend.domain.service;
+
+import com.cit.backend.domain.entity.Condominium;
+import com.cit.backend.domain.repository.CondominiumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+
+public class CondominiumService {
+    @Autowired
+    private CondominiumRepository condominiumRepository;
+
+    public Condominium save(Condominium condominium) {
+        return condominiumRepository.save(condominium);
+    }
+
+    public Condominium findById(Long id) {
+        return condominiumRepository.findById(id).orElse(null);
+    }
+
+    public void deleteById(Long id) {
+        condominiumRepository.deleteById(id);
+    }
+
+    public Condominium update(Condominium condominium) {
+        return condominiumRepository.save(condominium);
+    }
+}
