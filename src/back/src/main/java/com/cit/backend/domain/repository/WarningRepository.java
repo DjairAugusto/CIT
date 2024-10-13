733f6e0b2c1b8399cee219971e6eb0ef9e329b86
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-10-13 07:20:17 -0300

    Correção de importação

--- src/back/src/main/java/com/cit/backend/domain/repository/WarningRepository.java
+++ src/back/src/main/java/com/cit/backend/domain/repository/WarningRepository.java
@@ -1,7 +1,6 @@
 package com.cit.backend.domain.repository;
 
 import com.cit.backend.domain.entity.Warning;
-import com.cit.backend.domain.entity.WarningId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WarningRepository extends JpaRepository<Warning, Long> {
