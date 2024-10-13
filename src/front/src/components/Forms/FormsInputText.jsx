aeea860dca83ddf7ff0acad0da7f53cf49937b7d
jaoppb <joaopedroperes06@gmail.com>
2024-10-13 14:19:33 -0300

    correção do componente  FormsInputText

--- src/front/src/components/Forms/FormsInputText.jsx
+++ src/front/src/components/Forms/FormsInputText.jsx
@@ -1,5 +1,5 @@
 import { Eye, EyeOff } from "lucide-react";
-import React, { forwardRef, useState } from "react";
+import React, { forwardRef, useRef, useState } from "react";
 
 const Input = forwardRef(
 	({ LeftIcon, RightIcon, type, required, placeholder, ...rest }, ref) => {
@@ -23,7 +23,7 @@ const Input = forwardRef(
 		return (
 			<label className="flex items-center gap-4 px-4 py-2 border-sm border-medium bg-zinc-50 border-b-2 border-transparent rounded-md has-[:focus]:border-primary-1000 has-[:focus]:rounded-none transition-all duration-300 w-full">
 				{LeftIcon ? LeftIcon : ""}
-				<div className="relative w-full">
+				<div className="relative w-full group">
 					<input
 						className="bg-transparent h-12 w-full outline-none text-dark placeholder:text-dark text-zinc-700 placeholder:text-zinc-700 peer transition-colors pl-4"
 						type={
@@ -31,20 +31,18 @@ const Input = forwardRef(
 						}
 						{...rest}
 						ref={ref}
-						onChange={(e) =>
-							setActive(e.nativeEvent.target.value.length > 0)
-						}
+						onChange={(e) => setActive(e.target.value.length > 0) }
 					/>
 					<label
-						className={`text-zinc-600 absolute cursor-text left-0 top-1/4 peer-hover:top-0 hover:top-0 
-					peer-hover:text-xs  peer-focus:top-0 peer-focus:text-xs transition-all duration-300 truncate w-full overflow-hidden 
-					${isActive ? "text-xs top-0" : ""}`}
+						className={`
+							text-zinc-600 absolute cursor-text left-0
+							${isActive ? "text-xs top-0" : "top-1/4"}
+							group-hover:top-0 group-hover:text-xs
+							peer-focus:top-0 peer-focus:text-xs
+							transition-all duration-300 truncate w-full overflow-hidden
+						`}
 					>
-						{required ? (
-							<span className="text-red-500">* </span>
-						) : (
-							""
-						)}
+						{required && (<span className="text-red-500">* </span>)}
 						{placeholder}
 					</label>
 				</div>
