8a8f7dad00883ac62837ae9c8eacee5dd7be8990
jaoppb <joaopedroperes06@gmail.com>
2024-10-13 13:05:06 -0300

    refatoração do registro de apartamento utilizando o Forms.PageStep

--- src/front/src/pages/ApartmentRegistration/ProfileForm.jsx
+++ src/front/src/pages/ApartmentRegistration/ProfileForm.jsx
@@ -33,56 +33,44 @@ export default function ProfileForm() {
 		Object.assign(newProfiles, profiles);
 		const profile = newProfiles.find(value => value.id === target);
 		profile[key] = value;
-		
+
 		setProfiles(newProfiles);
 	}
 
 	return (
-		<div className="flex flex-col gap-6 justify-between items-center w-full h-fit">
-			<div className="w-full">
-				<h1 className="text-center text-4xl mb-4">
-					Cadastro de Perfis
-				</h1>
-				<div className="w-full flex items-center justify-center">
-					<div className="bg-zinc-400 h-[1px] w-full"></div>
-					<h6 className="px-4 whitespace-nowrap m-auto">
-						Apartamento XXX
-					</h6>
-					<div className="bg-zinc-400 h-[1px] w-full"></div>
-				</div>
+		<Forms.PageStep
+			title="Cadastro de Perfis"
+			subtitle="Apartamento XXX"
+		>
+			<button
+				className="text-primary-1000 block mb-2"
+				onClick={addProfile}
+			>+ Adicionar Perfil</button>
+			<div className="flex flex-col gap-6 overflow-y-auto max-h-[600px]">
+				{
+					profiles.map((profile, index) => (
+						<DropMenu
+							key={profile.id}
+							text={index === 0 ? "Perfil Principal" : `Pefil Secundário ${index}`}
+							removeCallback={index === 0 ? null : () => removeProfile(profile.id)}
+						>
+							<div className="flex gap-4 flex-col">
+								<Forms.InputText
+									placeholder="EMAIL"
+									onChange={e => modifyProfile(profile.id, ["email", e.nativeEvent.srcElement.value])}
+									required
+								/>
+								<Forms.InputText
+									placeholder="SENHA"
+									type="password"
+									onChange={e => modifyProfile(profile.id, ["password", e.nativeEvent.srcElement.value])}
+									required
+								/>
+							</div>
+						</DropMenu>
+					))
+				}
 			</div>
-
-			<div className="w-full h-fit">
-				<button
-					className="text-primary-1000 block mb-2"
-					onClick={addProfile}
-				>+ Adicionar Perfil</button>
-				<div className="flex flex-col gap-6 overflow-y-auto max-h-[600px]">
-					{
-						profiles.map((profile, index) => (
-							<DropMenu
-								key={profile.id}
-								text={index === 0 ? "Perfil Principal" : `Pefil Secundário ${index}`}
-								removeCallback={index === 0 ? null : () => removeProfile(profile.id)}
-							>
-								<div className="flex gap-4 flex-col">
-									<Forms.InputText
-										placeholder="EMAIL"
-										onChange={e => modifyProfile(profile.id, ["email", e.nativeEvent.srcElement.value])}
-										required
-									/>
-									<Forms.InputText
-										placeholder="SENHA"
-										type="password"
-										onChange={e => modifyProfile(profile.id, ["password", e.nativeEvent.srcElement.value])}
-										required
-									/>
-								</div>
-							</DropMenu>
-						))
-					}
-				</div>
-			</div>
-		</div>
+		</Forms.PageStep>
 	);
 }
