0afb6fed2c1e091b3d05612c33694e46eff2bd36
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-10-18 02:27:01 -0300

    Implementação do InputMask

---
+++ src/front/src/components/Forms/FormsInputMask.jsx
@@ -0,0 +1,62 @@
+import { Eye, EyeOff } from "lucide-react";
+import React, {forwardRef, useRef, useState} from "react";
+import InputMask from 'react-input-mask';
+
+const Input = forwardRef(
+	({ LeftIcon, RightIcon, type, required, placeholder, mask, ...rest }, ref) => {
+		const [seePassword, setSeePassoword] = useState(false);
+		const [isActive, setActive] = useState(false);
+
+		function togglePassword() {
+			setSeePassoword(!seePassword);
+		}
+
+		if (type === "password")
+			RightIcon = (
+				<div
+					onClick={togglePassword}
+					className="text-medium cursor-pointer text-zinc-700"
+				>
+					{seePassword ? <Eye /> : <EyeOff />}
+				</div>
+			);
+
+		return (
+			<label className="flex items-center gap-4 px-4 py-2 border-sm border-medium bg-zinc-50 border-b-2 border-transparent rounded-md has-[:focus]:border-primary-1000 has-[:focus]:rounded-none transition-all duration-300 w-full">
+				{LeftIcon ? LeftIcon : ""}
+				<div className="relative w-full group">
+					<InputMask mask={mask} onChange={(e) => setActive(e.target.value.length > 0)}>
+						{(inputProps) => (
+                            <input
+                                className="bg-transparent h-12 w-full outline-none text-dark placeholder:text-dark text-zinc-700 placeholder:text-zinc-700 peer transition-colors pl-4"
+                                type={
+                                    type === "password" && seePassword ? "text" : type
+                                }
+                                {...inputProps}
+                                {...rest}
+                                ref={ref}
+
+                            />
+                        )}
+					</InputMask>
+					<label
+						className={`
+							text-zinc-600 absolute cursor-text left-0
+							${isActive ? "text-xs top-0" : "top-1/4"}
+							group-hover:top-0 group-hover:text-xs
+							peer-focus:top-0 peer-focus:text-xs
+							transition-all duration-300 truncate w-full overflow-hidden
+						`}
+					>
+						{required && (<span className="text-red-500">* </span>)}
+						{placeholder}
+					</label>
+				</div>
+				{RightIcon ? RightIcon : ""}
+			</label>
+		);
+	}
+);
+Input.displayName = "Input";
+
+export default Input;
