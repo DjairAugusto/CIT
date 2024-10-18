57ad795628c2d75f68cf1745e970b78932b2b019
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-10-18 03:16:44 -0300

    Inicio da implementação do envio do formulario

--- src/front/src/components/Forms/FormsInputMask.jsx
+++ src/front/src/components/Forms/FormsInputMask.jsx
@@ -3,13 +3,20 @@ import React, {forwardRef, useRef, useState} from "react";
 import InputMask from 'react-input-mask';
 
 const Input = forwardRef(
-	({ LeftIcon, RightIcon, type, required, placeholder, mask, ...rest }, ref) => {
+	({ LeftIcon, RightIcon, type, required, placeholder, mask, onChange, ...rest }, ref) => {
 		const [seePassword, setSeePassoword] = useState(false);
 		const [isActive, setActive] = useState(false);
 
 		function togglePassword() {
 			setSeePassoword(!seePassword);
 		}
+
+		const handleChange = (e) => {
+            setActive(e.target.value.length > 0);
+            if (onChange) {
+                onChange(e);
+            }
+        };
 
 		if (type === "password")
 			RightIcon = (
@@ -25,24 +32,24 @@ const Input = forwardRef(
 			<label className="flex items-center gap-4 px-4 py-2 border-sm border-medium bg-zinc-50 border-b-2 border-transparent rounded-md has-[:focus]:border-primary-1000 has-[:focus]:rounded-none transition-all duration-300 w-full">
 				{LeftIcon ? LeftIcon : ""}
 				<div className="relative w-full group">
-					<InputMask mask={mask} onChange={(e) => setActive(e.target.value.length > 0)}>
+					<InputMask
+						mask={mask}
+						onChange={handleChange}
+						{...rest}
+						ref={ref}
+						type={type === "password" && seePassword ? "text" : type}
+					>
 						{(inputProps) => (
                             <input
                                 className="bg-transparent h-12 w-full outline-none text-dark placeholder:text-dark text-zinc-700 placeholder:text-zinc-700 peer transition-colors pl-4"
-                                type={
-                                    type === "password" && seePassword ? "text" : type
-                                }
                                 {...inputProps}
-                                {...rest}
-                                ref={ref}
-
                             />
                         )}
 					</InputMask>
 					<label
 						className={`
 							text-zinc-600 absolute cursor-text left-0
-							${isActive ? "text-xs top-0" : "top-1/4"}
+							${(isActive || rest.value.length > 0) ? "text-xs top-0" : "top-1/4"}
 							group-hover:top-0 group-hover:text-xs
 							peer-focus:top-0 peer-focus:text-xs
 							transition-all duration-300 truncate w-full overflow-hidden
