d50127c06b98364b34340d9eb2f07b6ea22d0191
Kaio Mayer <99258505+kaiomayer@users.noreply.github.com>
2024-10-20 18:09:23 -0300

    Update ApartmentService.java

---
+++ src/back/src/main/java/com/cit/backend/domain/service/ApartmentService.java
@@ -0,0 +1,33 @@
+package com.cit.backend.domain.service;
+
+import com.cit.backend.domain.entity.Apartment;
+import com.cit.backend.domain.repository.ApartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+
+public class ApartmentService {
+    @Autowired
+    private ApartmentService apartmentService;
+
+    public Apartment save(Apartment apartment){
+        return apartmentRepository.save(apartment);
+    }
+
+    public Apartment findById(Long id) {
+        return apartmentRepository.findById(id).orElse(null);
+    }
+
+    public void deleteById(Long id) {
+        apartmentRepository.deleteById(id)
+    }
+
+    public Apartment update(Apartment apartment) {
+        return apartmentRepository.save(apartment);
+    }
+
+}
+
