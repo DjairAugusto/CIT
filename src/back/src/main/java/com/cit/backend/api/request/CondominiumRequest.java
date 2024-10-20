517d919b35a5ca550514568c378b72d8a433633f
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-10-20 07:56:27 -0300

    Criação de DTO's

--- src/back/src/main/java/com/cit/backend/api/request/CondominiumRequest.java
+++ src/back/src/main/java/com/cit/backend/api/request/CondominiumRequest.java
@@ -1,6 +1,10 @@
 package com.cit.backend.api.request;
 
+import com.cit.backend.api.validator.Phone;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,16 +15,24 @@ import org.hibernate.validator.constraints.br.CNPJ;
 @AllArgsConstructor
 public class CondominiumRequest {
     @NotBlank(message = "Name is mandatory")
+    @Size(min = 3, max = 80, message = "Name must have between 3 and 80 characters")
     private String name;
 
     @NotBlank(message = "CNPJ is mandatory")
     @CNPJ(message = "Invalid CNPJ")
     private String cnpj;
 
-
+    @NotNull(message = "Blocks is mandatory")
+    @PositiveOrZero(message = "Blocks must be greater than zero")
     private int blocks;
 
+    @NotNull(message = "Units is mandatory")
+    @PositiveOrZero(message = "Units must be greater than zero")
     private int units;
 
+    @NotNull(message = "Apartments is mandatory")
+    @PositiveOrZero(message = "Apartments must be greater than zero")
     private int apartments;
+    
+    private AddressRequest address;
 }
---
+++ src/back/src/main/java/com/cit/backend/api/response/EmployeeResponse.java
@@ -0,0 +1,4 @@
+package com.cit.backend.api.response;
+
+public class EmployeeResponse {
+}
