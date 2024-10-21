24fe520023b87e2f88f2c83ee50e54e3e2745c9e
Djair Augusto <dagsoares@sga.pucminas.br>
2024-10-21 20:12:06 -0300

    Mudanças no front Cadastro funcionário - Parte de perfis alterado para apenas um unico perfil primário e movido para a primeira pagina

--- src/front/src/pages/FunctionaryRegistration/index.jsx
+++ src/front/src/pages/FunctionaryRegistration/index.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import EmployeeForm from "./EmployeeForm";
 import VehicleForm from "./VehicleForm";
-import ProfileForm from "./ProfileForm";
 import { Forms } from "../../components/Forms";
 
 export default function ApartmentRegistration() {
-	const steps = [<EmployeeForm />, <VehicleForm />, <ProfileForm />];
+	const steps = [<EmployeeForm />, <VehicleForm />];
 
 	return (
 		<Forms.Page
