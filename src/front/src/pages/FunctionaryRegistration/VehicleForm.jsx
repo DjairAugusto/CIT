8e51f897f80af3af5a354fee437ada5ed8cb6e98
Djair Augusto <dagsoares@sga.pucminas.br>
2024-10-21 20:12:06 -0300

    Mudanças no front Cadastro funcionário - Parte de perfis alterado para apenas um unico perfil primário e movido para a primeira pagina

--- src/front/src/pages/FunctionaryRegistration/VehicleForm.jsx
+++ src/front/src/pages/FunctionaryRegistration/VehicleForm.jsx
@@ -4,83 +4,86 @@ import { Forms } from "../../components/Forms";
 import useObjectArray from "../../hooks/useObjectArray";
 
 const vehicleOptions = [
-	{
-		value: "car",
-		text: "CARRO"
-	},
-	{
-		value: "motocycle",
-		text: "MOTO"
-	},
-	{
-		value: "other",
-		text: "OUTRO"
-	}
+    {
+        value: "car",
+        text: "CARRO"
+    },
+    {
+        value: "motocycle",
+        text: "MOTO"
+    },
+    {
+        value: "other",
+        text: "OUTRO"
+    }
 ];
 
 export default function VehicleForm() {
-	const {
-		array: vehicles,
-		addOne: addVehicle,
-		removeOne: removeVehicle,
-		modifyOne: modifyVehicle
-	} = useObjectArray({
-		entries: [
-			"type",
-			"model",
-			"color",
-			"plate",
-		]
-	});
+    const {
+        array: vehicles,
+        addOne: addVehicle,
+        removeOne: removeVehicle,
+        modifyOne: modifyVehicle
+    } = useObjectArray({
+        entries: [
+            "type",
+            "model",
+            "color",
+            "plate",
+        ]
+    });
 
-	return (
-		<Forms.PageStep
-			title="Cadastro de Veículos"
-			subtitle="Funcionário XXX"
-		>
-			<div className="w-full h-fit">
-				<button
-					className="text-primary-1000 block mb-2"
-					onClick={addVehicle}
-				>+ Adicionar Veículo</button>
-				<div className="flex flex-col gap-6 overflow-y-auto max-h-[600px]">
-					{
-						vehicles.map(vehicle => (
-							<DropMenu
-								key={vehicle.id}
-								text={vehicle.model ? vehicle.model : "Modelo do Carro"}
-								removeCallback={() => removeVehicle(vehicle.id)}
-							>
-								<div className="flex gap-4 flex-col">
-									<Forms.Select
-										placeholder="TIPO"
-										onChange={e => modifyVehicle(vehicle.id, ["type", e.nativeEvent.srcElement.value])}
-										options={vehicleOptions}
-										required={true}
-									/>
-									<Forms.InputText
-										placeholder="MODELO"
-										onChange={e => modifyVehicle(vehicle.id, ["model", e.nativeEvent.srcElement.value])}
-										required
-									/>
+    const handleSubmit = () => {
+        // Lógica para cadastrar os veículos
+        console.log("Veículos cadastrados:", vehicles);
+    };
 
-									<Forms.InputText
-										placeholder="COR"
-										onChange={e => modifyVehicle(vehicle.id, ["color", e.nativeEvent.srcElement.value])}
-										required
-									/>
-
-									<Forms.InputText
-										placeholder="PLACA"
-										onChange={e => modifyVehicle(vehicle.id, ["plate", e.nativeEvent.srcElement.value])}
-										required
-									/>
-								</div>
-							</DropMenu>
-						))
-					}
-				</div>
-			</div>
-		</Forms.PageStep>
-	);
+    return (
+        <Forms.PageStep
+            title="Cadastro de Veículos"
+            subtitle="Funcionário XXX"
+        >
+            <div className="w-full h-fit">
+                <button
+                    className="text-primary-1000 block mb-2"
+                    onClick={addVehicle}
+                >+ Adicionar Veículo</button>
+                <div className="flex flex-col gap-6 overflow-y-auto max-h-[600px]">
+                    {
+                        vehicles.map(vehicle => (
+                            <DropMenu
+                                key={vehicle.id}
+                                text={vehicle.model ? vehicle.model : "Modelo do Carro"}
+                                removeCallback={() => removeVehicle(vehicle.id)}
+                            >
+                                <div className="flex gap-4 flex-col">
+                                    <Forms.Select
+                                        placeholder="TIPO"
+                                        onChange={e => modifyVehicle(vehicle.id, ["type", e.nativeEvent.srcElement.value])}
+                                        options={vehicleOptions}
+                                        required={true}
+                                    />
+                                    <Forms.InputText
+                                        placeholder="MODELO"
+                                        onChange={e => modifyVehicle(vehicle.id, ["model", e.nativeEvent.srcElement.value])}
+                                        required
+                                    />
+                                    <Forms.InputText
+                                        placeholder="COR"
+                                        onChange={e => modifyVehicle(vehicle.id, ["color", e.nativeEvent.srcElement.value])}
+                                        required
+                                    />
+                                    <Forms.InputText
+                                        placeholder="PLACA"
+                                        onChange={e => modifyVehicle(vehicle.id, ["plate", e.nativeEvent.srcElement.value])}
+                                        required
+                                    />
+                                </div>
+                            </DropMenu>
+                        ))
+                    }
+                </div>
+            </div>
+        </Forms.PageStep>
+    );
 }
