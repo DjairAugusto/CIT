cc558ef256bcd8ff328cfc707daa3f8e47968c6d
João Victor <111539495+FhaitPCP@users.noreply.github.com>
2024-10-21 18:32:01 -0300

    Adicionado o Response e o Controler do Employee

--- src/back/src/main/java/com/cit/backend/api/controller/EmployeeController.java
+++ src/back/src/main/java/com/cit/backend/api/controller/EmployeeController.java
@@ -2,13 +2,16 @@ package com.cit.backend.api.controller;
 
 import com.cit.backend.api.mapper.EmployeeMapper;
 import com.cit.backend.api.request.AdminRequest;
+import com.cit.backend.api.request.EmployeeRequest;
 import com.cit.backend.api.response.EmployeeResponse;
 import com.cit.backend.domain.entity.Employee;
 import com.cit.backend.domain.service.CondominiumService;
 import com.cit.backend.domain.service.EmployeeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,15 +26,11 @@ public class EmployeeController {
     @Autowired
     private EmployeeMapper employeeMapper;
 
-
-
-    @postMapping("/admin")
-    public ResponseEntity<AdminRequest> createEmployee(@Valid @RequestBody AdminRequest request) {
-        EmployeeMapper employeeMapper;
-        Employee employee = employeeMapper.toEmployee(request);
-        Employee employeeSaved = employeeService.save(employee);
-        EmployeeResponse response = employeeMapper.toEmployeeResponse(employeeSaved);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    @PostMapping
+    public ResponseEntity<EmployeeResponse> createEmployee(@Valid @RequestBody EmployeeRequest employeeRequest) {
+        Employee employee = employeeMapper.toEmployee(employeeRequest);
+        Employee employeeSave = employeeService.save(employee);
+        EmployeeResponse employeeResponse = employeeMapper.toEmployeeResponse(employeeSave);
+        return ResponseEntity.status(HttpStatus.CREATED).body(employeeResponse);
     }
 }
--- src/back/src/main/java/com/cit/backend/api/response/EmployeeResponse.java
+++ src/back/src/main/java/com/cit/backend/api/response/EmployeeResponse.java
@@ -1,4 +1,18 @@
 package com.cit.backend.api.response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
 public class EmployeeResponse {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String role;
+
 }
