977d927b59b61539c3eab9e03c33cd5a4eca45a4
jaoppb <joaopedroperes06@gmail.com>
2024-10-20 22:05:29 -0300

    correção do JWTValidator

--- src/back/src/main/java/com/cit/backend/api/validator/JWTValidator.java
+++ src/back/src/main/java/com/cit/backend/api/validator/JWTValidator.java
@@ -1,14 +1,10 @@
 package com.cit.backend.api.validator;
 
-import com.cit.backend.domain.service.JWTApartmentService;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class JWTValidator implements ConstraintValidator<JWT, String> {
-    @Autowired
-    private JWTApartmentService jwtApartmentService;
-
     @Override
     public void initialize(JWT constraintAnnotation) {
     }
