6848d62d47bf8aff53d5f1cef2bd8f0d0fb17f7c
bgluis <luishenrique.gbarbosa21@gmail.com>
2024-10-22 14:11:07 -0300

    Criação do registro do administrador

---
+++ src/back/src/main/java/com/cit/backend/api/response/AdminResponse.java
@@ -0,0 +1,19 @@
+package com.cit.backend.api.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminResponse {
+    private Long id;
+    private String role;
+    private String cpf;
+    private String permission;
+    private String name;
+
+}
