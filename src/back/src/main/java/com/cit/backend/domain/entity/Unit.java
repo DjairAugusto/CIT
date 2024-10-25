4664b78609dbf405d8d9d33956b91377e5ed61ec
jaoppb <joaopedroperes06@gmail.com>
2024-10-25 12:52:26 -0300

    id de Unit alterado para Long para consistência

--- src/back/src/main/java/com/cit/backend/domain/entity/Unit.java
+++ src/back/src/main/java/com/cit/backend/domain/entity/Unit.java
@@ -15,7 +15,7 @@ public class Unit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private Long id;
 
     @Column(nullable = false)
     private int number;
