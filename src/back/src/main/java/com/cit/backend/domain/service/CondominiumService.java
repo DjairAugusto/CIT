26d0632f7fbc4c73c00f8d84179bd2a2371aceb1
Luis Henrique G Barbosa <92602646+BGLuis@users.noreply.github.com>
2024-10-26 04:42:55 -0300

    Inicio da geração do comdominio

--- src/back/src/main/java/com/cit/backend/domain/service/CondominiumService.java
+++ src/back/src/main/java/com/cit/backend/domain/service/CondominiumService.java
@@ -1,6 +1,10 @@
 package com.cit.backend.domain.service;
 
+import com.cit.backend.domain.entity.Apartment;
+import com.cit.backend.domain.entity.Block;
 import com.cit.backend.domain.entity.Condominium;
+import com.cit.backend.domain.entity.Unit;
+import com.cit.backend.domain.repository.BlockRepository;
 import com.cit.backend.domain.repository.CondominiumRepository;
 import com.cit.backend.exceptions.UniqueColumnAlreadyExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +17,27 @@ public class CondominiumService {
     @Autowired
     private CondominiumRepository condominiumRepository;
 
+    @Autowired
+    private BlockRepository blockService;
+
     public Condominium save(Condominium condominium) {
         if (condominiumRepository.findByCnpj(condominium.getCnpj()) != null) {
             throw new UniqueColumnAlreadyExistsException("CNPJ Already Exists");
+        }
+
+        Condominium condominiumSaved = condominiumRepository.save(condominium);
+
+        for (int i = 0; i < condominium.getBlocks(); i++) {
+            Block block = new Block();
+            for (int j = 0; j < condominium.getUnits(); j++) {
+                Unit unit = new Unit();
+                for (int k = 0; k < condominium.getApartments(); k++) {
+                    Apartment apartment = new Apartment();
+
+                }
+            }
+
+            blockService.save(block);
         }
 
         return condominiumRepository.save(condominium);
