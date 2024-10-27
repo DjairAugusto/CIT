ba6348141b4fe0f6ff8511e1e19db76bac095af2
jaoppb <joaopedroperes06@gmail.com>
2024-10-27 11:56:05 -0300

    fixed bad resolved merge

--- src/back/src/main/java/com/cit/backend/api/intra/message/RestErroMessage.java
+++ src/back/src/main/java/com/cit/backend/api/intra/message/RestErroMessage.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
-<<<<<<< HEAD
 import org.springframework.http.HttpStatusCode;
-=======
->>>>>>> e8276bb (Separação de comteudo/criação da pagina api)
 
 @AllArgsConstructor
 @Getter
@@ -15,12 +12,9 @@ import org.springframework.http.HttpStatusCode;
 public class RestErroMessage {
     private HttpStatus status;
     private String message;
-<<<<<<< HEAD
 
     public RestErroMessage(HttpStatusCode status, String message) {
         this.status = HttpStatus.valueOf(status.value());
         this.message = message;
     }
-=======
->>>>>>> e8276bb (Separação de comteudo/criação da pagina api)
 }
