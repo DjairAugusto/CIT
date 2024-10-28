44d439049ebd7f0b723baa77b7a1fd28d60bf96b
jaoppb <joaopedroperes06@gmail.com>
2024-10-28 17:08:14 -0300

    base da SideBar implementada

---
+++ src/front/src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SideBar() {
+	return (
+		<div className="w-28 h-full bg-gray-200">
+			<img className="w-full" src="/icon-light.webp" alt="site icon" />
+		</div>
+	);
+}
