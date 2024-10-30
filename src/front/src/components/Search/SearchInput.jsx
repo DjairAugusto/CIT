13329119a3343ecc11b35ec968ad6540a72f24c2
jaoppb <joaopedroperes06@gmail.com>
2024-10-30 16:10:22 -0300

    fixed search input on really really small screens

--- src/front/src/components/Search/SearchInput.jsx
+++ src/front/src/components/Search/SearchInput.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function SearchInput({ placeholder }) {
 	return (
 		<input
-			className="bg-gray-300 rounded-2xl px-3 text-lg flex-1"
+			className="bg-gray-300 rounded-2xl px-3 text-lg flex-1 min-w-0"
 			type="text"
 			placeholder={placeholder}
 		/>
