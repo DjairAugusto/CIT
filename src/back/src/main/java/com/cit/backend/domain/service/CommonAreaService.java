16eebedf04e644a1ccec9b010e6679f38a46f84f
jaoppb <joaopedroperes06@gmail.com>
2024-11-02 19:49:18 -0300

    added save method in CommonAreaService

--- src/back/src/main/java/com/cit/backend/domain/service/CommonAreaService.java
+++ src/back/src/main/java/com/cit/backend/domain/service/CommonAreaService.java
@@ -1,5 +1,6 @@
 package com.cit.backend.domain.service;
 
+import com.cit.backend.domain.entity.CommonArea;
 import com.cit.backend.domain.repository.CommonAreaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,4 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class CommonAreaService {
     @Autowired
     private CommonAreaRepository commonAreaRepository;
+
+    public CommonArea save(CommonArea commonArea) {
+        return commonAreaRepository.save(commonArea);
+    }
 }
