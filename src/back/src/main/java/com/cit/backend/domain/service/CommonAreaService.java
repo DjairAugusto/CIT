cb68585902ebf92d1fcfcec45e67b9566198fb98
jaoppb <joaopedroperes06@gmail.com>
2024-11-02 19:49:41 -0300

    added getCommonAreasByCondominiumId in CommonAreaService

--- src/back/src/main/java/com/cit/backend/domain/service/CommonAreaService.java
+++ src/back/src/main/java/com/cit/backend/domain/service/CommonAreaService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class CommonAreaService {
@@ -14,5 +16,9 @@ public class CommonAreaService {
 
     public CommonArea save(CommonArea commonArea) {
         return commonAreaRepository.save(commonArea);
+    }
+
+    public List<CommonArea> getCommonAreasByCondominiumId(long condominiumId) {
+        return commonAreaRepository.findAllByCondominiumId(condominiumId);
     }
 }
