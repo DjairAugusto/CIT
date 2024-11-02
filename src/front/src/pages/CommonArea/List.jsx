5a49df9f658fcaf1f937e8fe0d58f27218082405
jaoppb <joaopedroperes06@gmail.com>
2024-11-01 23:40:11 -0300

    no need to fragment wrapper

--- src/front/src/pages/CommonArea/List.jsx
+++ src/front/src/pages/CommonArea/List.jsx
@@ -4,26 +4,24 @@ import ImageCard from "../../components/ImageCard";
 
 export default function CommonAreaList({ commonAreas, setFocused, role }) {
 	return (
-		<>
-			<main className="w-full h-full p-4 flex flex-col bg-white">
-				<div className="flex items-center flex-col mb-4">
-					<h1 className="text-center text-4xl mb-2">Áreas Comuns</h1>
-					<div className="bg-zinc-400 h-[1px] w-1/4"></div>
-				</div>
-				<SearchBar role={role} />
+		<main className="w-full h-full p-4 flex flex-col bg-white">
+			<div className="flex items-center flex-col mb-4">
+				<h1 className="text-center text-4xl mb-2">Áreas Comuns</h1>
+				<div className="bg-zinc-400 h-[1px] w-1/4"></div>
+			</div>
+			<SearchBar role={role} />
 
-				<div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 place-content-stretch w-full mt-4 gap-4 max-h-full overflow-auto">
-					{commonAreas.map((commonArea) => (
-						<ImageCard
-							imgSrc={commonArea.img}
-							title={commonArea.name}
-							className="w-full h-full min-h-60"
-							buttonText="Ver Detalhes"
-							buttonAction={() => setFocused(commonArea)}
-						/>
-					))}
-				</div>
-			</main>
-		</>
+			<div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 place-content-stretch w-full mt-4 gap-4 max-h-full overflow-auto">
+				{commonAreas.map((commonArea) => (
+					<ImageCard
+						imgSrc={commonArea.img}
+						title={commonArea.name}
+						className="w-full h-full min-h-60"
+						buttonText="Ver Detalhes"
+						buttonAction={() => setFocused(commonArea)}
+					/>
+				))}
+			</div>
+		</main>
 	);
 }
