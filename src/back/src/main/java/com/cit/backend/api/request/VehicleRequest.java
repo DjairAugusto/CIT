663bd16a52f4a97b1e84b7f8a67e7930acaed46f
jaoppb <joaopedroperes06@gmail.com>
2024-11-03 14:42:56 -0300

    fixed annotations from EmployeeRequest and VehicleRequest

--- src/back/src/main/java/com/cit/backend/api/request/VehicleRequest.java
+++ src/back/src/main/java/com/cit/backend/api/request/VehicleRequest.java
@@ -1,29 +1,27 @@
 package com.cit.backend.api.request;
 
-import com.cit.backend.api.validator.JWT;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class VehicleRequest {
-
-    @NotBlank(message = "Type is mandatory");
+    @NotBlank(message = "Type is mandatory")
     private String type;
 
-    @NotBlank(message = "Model is mandatory");
+    @NotBlank(message = "Model is mandatory")
     private String model;
 
-    @NotBlank(message = "Color is mandatory");
+    @NotBlank(message = "Color is mandatory")
     private String color;
 
-    @NotBlank(message = "Plate is mandatory");
-    @Size(min = 6, max = 7, message = "Plate must be between 6 and 7 characters");
+    @NotBlank(message = "Plate is mandatory")
+    @Size(min = 6, max = 7, message = "Plate must be between 6 and 7 characters")
     private String plate;
-
 }
