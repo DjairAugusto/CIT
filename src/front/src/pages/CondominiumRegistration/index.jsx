49c508300a341e1e8f1fb00212c4a6db32808088
jaoppb <joaopedroperes06@gmail.com>
2024-11-03 00:27:44 -0300

    fixed duplicate const variable and typo

--- src/front/src/pages/CondominiumRegistration/index.jsx
+++ src/front/src/pages/CondominiumRegistration/index.jsx
@@ -1,10 +1,10 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import CondominiumForm from "./CondominiumForm";
 import { Forms } from "../../components/Forms";
 import AdminForm from "./AdminForm";
-import usePageTitle from '../../utils/usePageTitle';
+import usePageTitle from "../../utils/usePageTitle";
 
-const formsTemplete = {
+const formsTemplate = {
 	condominium: {
 		name: "",
 		cnpj: "",
@@ -18,69 +18,66 @@ const formsTemplete = {
 		password: "",
 		confirm: "",
 	},
-}
+};
 
 // TODO Adicionar validação de CPF e CNPJ
 // TODO Adicionar validação de email
 // TODO Adicionar validação de senha
 // TODO Adicionar a connexão com a API para enviar os dados
 
-const formsTemplete = {
-	condominium: {
-		name: "",
-		cnpj: "",
-		blocks: "",
-		floors: "",
-	},
-	employees: [],
-}
-
 export default function CondominiumRegistration() {
-	const [data, setData] = React.useState(formsTemplete);
-	const emptyFieldsCondominium = []
-	const emptyFieldsAdmin = []
+	const [data, setData] = React.useState(formsTemplate);
+	const emptyFieldsCondominium = [];
+	const emptyFieldsAdmin = [];
 	// const [emptyFieldsCondominium, setEmptyFieldsCondominium] = React.useState({});
 	// const [emptyFieldsAdmin, setEmptyFieldsAdmin] = React.useState({});
 
 	usePageTitle("Cadastro de Condomínio");
 
 	useEffect(() => {
-		console.log(formsTemplete);
-	},[data])
-	
+		console.log(formsTemplate);
+	}, [data]);
+
 	function fieldsEmpty(fieldsData) {
 		const fields = Object.keys(fieldsData)
-		.filter(key => !fieldsData[key])
-		.map(key => ({ [key]: 'Campo obrigatório' }));
+			.filter((key) => !fieldsData[key])
+			.map((key) => ({ [key]: "Campo obrigatório" }));
 		return fields;
-
 	}
-	function validateCondominiumForm()  {
-		let fieldsErro;
-		
-		fieldsErro += fieldsEmpty(data.condominium);
-		return fieldsErro.length === 0;
+	function validateCondominiumForm() {
+		let fieldsErrors;
+
+		fieldsErrors += fieldsEmpty(data.condominium);
+		return fieldsErrors.length === 0;
 	}
 
 	function validateAdminForm() {
-		let fieldsErro;
-		fieldsErro = fieldsEmpty(data.admin);
-		return fieldsErro.length === 0;
+		let fieldsErrors;
+		fieldsErrors = fieldsEmpty(data.admin);
+		return fieldsErrors.length === 0;
 	}
 
-    const updateFieldHandler = (section, field, value) => {
-        setData((prevData) => ({
-            ...prevData,
-            [section]: {
-                ...prevData[section],
-                [field]: value,
-            },
-        }));
-    }
-	
+	const updateFieldHandler = (section, field, value) => {
+		setData((prevData) => ({
+			...prevData,
+			[section]: {
+				...prevData[section],
+				[field]: value,
+			},
+		}));
+	};
+
 	const steps = [
-		<CondominiumForm data={data.condominium} updateFieldHandler={updateFieldHandler} subtitle={data.condominium.name || "Nome do condominio"} />,
-		<AdminForm data={data.admin} updateFieldHandler={updateFieldHandler} subtitle={data.condominium.name || "Nome do condominio"}/>
+		<CondominiumForm
+			data={data.condominium}
+			updateFieldHandler={updateFieldHandler}
+			subtitle={data.condominium.name || "Nome do condominio"}
+		/>,
+		<AdminForm
+			data={data.admin}
+			updateFieldHandler={updateFieldHandler}
+			subtitle={data.condominium.name || "Nome do condominio"}
+		/>,
 	];
 
 	return (
