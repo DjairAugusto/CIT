40e8c5efcd69197ba114077f8eefc579298fb5b6
jaoppb <joaopedroperes06@gmail.com>
2024-11-03 16:39:17 -0300

    added CommonAreaSchedule Request and Response

---
+++ src/back/src/main/java/com/cit/backend/api/request/CommonAreaScheduleRequest.java
@@ -0,0 +1,10 @@
+package com.cit.backend.api.request;
+
+import java.time.DayOfWeek;
+
+public class CommonAreaScheduleRequest {
+    private Long id;
+    private String startTime;
+    private String endTime;
+    private DayOfWeek dayOfWeek;
+}
