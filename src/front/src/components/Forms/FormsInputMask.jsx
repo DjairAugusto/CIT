cde4583cd8387fe37afb8a472dd1691e3dc461a7
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-11-04 02:41:05 -0300

    Exibição do erro

--- src/front/src/components/Forms/FormsInputMask.jsx
+++ src/front/src/components/Forms/FormsInputMask.jsx
@@ -3,30 +3,26 @@ import React, {forwardRef, useState} from "react";
 import InputMask from 'react-input-mask';
 
 const Input = forwardRef(
-	({ LeftIcon, RightIcon, type, required, placeholder, mask, onChange, ...rest }, ref) => {
-		const [seePassword, setSeePassoword] = useState(false);
+	({ LeftIcon, RightIcon, type, required, placeholder, mask, onChange, onBlur, onFocus, error, ...rest }, ref) => {
 		const [isActive, setActive] = useState(false);
-
-		function togglePassword() {
-			setSeePassoword(!seePassword);
-		}
+		const [isFocus, setFocus] = useState(false);
+		const [isFistFocus, setFistFocus] = useState(false);
 
 		const handleChange = (e) => {
             setActive(e.target.value.length > 0);
-            if (onChange) {
-                onChange(e);
-            }
+            if (onChange) onChange(e);
+		};
+		
+        const handleFocus = (e) => {
+			setFocus(true);
+			setFistFocus(true);
+			if(onFocus) onFocus(e);
         };
 
-		if (type === "password")
-			RightIcon = (
-				<div
-					onClick={togglePassword}
-					className="text-medium cursor-pointer text-zinc-700"
-				>
-					{seePassword ? <Eye /> : <EyeOff />}
-				</div>
-			);
+        const handleBlur = (e) => {
+            setFocus(false);
+            if (onBlur) onBlur(e);
+        };
 
 		return (
 			<label className="flex items-center gap-4 px-4 py-2 border-sm border-medium bg-zinc-50 border-b-2 border-transparent rounded-md has-[:focus]:border-primary-1000 has-[:focus]:rounded-none transition-all duration-300 w-full">
@@ -34,17 +30,15 @@ const Input = forwardRef(
 				<div className="relative w-full group">
 					<InputMask
 						mask={mask}
-						onChange={handleChange}
+                        onChange={handleChange}
+                        onFocus={handleFocus}
+                        onBlur={handleBlur}
 						{...rest}
 						ref={ref}
-						type={type === "password" && seePassword ? "text" : type}
 					>
 						{(inputProps) => (
                             <input
                                 className="bg-transparent h-12 w-full outline-none text-dark placeholder:text-dark text-zinc-700 placeholder:text-zinc-700 peer transition-colors pl-4"
-                                type={
-                                    type === "password" && seePassword ? "text" : type
-                                }
                                 {...inputProps}
                                 {...rest}
                                 ref={ref}
@@ -63,6 +57,14 @@ const Input = forwardRef(
 					>
 						{required && (<span className="text-red-500">* </span>)}
 						{placeholder}
+												<label
+							className={`
+								${(isFocus && isFistFocus) ? "hidden" : ""}
+								text-red-500 ml-4	
+							`}
+						>
+							{(error && isFistFocus) && `*${error}`}
+						</label>
 					</label>
 				</div>
 				{RightIcon ? RightIcon : ""}
