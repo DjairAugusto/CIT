9cca5530f2a68b6cc8231e05f084e9a3f0938111
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-11-04 05:38:39 -0300

    Implementação de regras de cors

---
+++ src/back/src/main/java/com/cit/backend/api/config/webConfig.java
@@ -0,0 +1,21 @@
+package com.cit.backend.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class webConfig implements WebMvcConfigurer{
+
+    public void addCorsMappings(CorsRegistry registry) {
+        //TODO restringir origens
+        //TODO refatora
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
