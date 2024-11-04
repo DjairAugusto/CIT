66940df13d425270eed090364a5860954b310f62
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-11-04 05:44:49 -0300

    Correções no formulario

--- src/front/src/pages/CondominiumRegistration/index.jsx
+++ src/front/src/pages/CondominiumRegistration/index.jsx
@@ -1,77 +1,155 @@
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 import CondominiumForm from "./CondominiumForm";
 import { Forms } from "../../components/Forms";
 import AdminForm from "./AdminForm";
 import usePageTitle from "../../utils/usePageTitle";
+import validCnpj from "../../utils/validCnpj";
+import validCpf from "../../utils/validCpf";
+import validateEmail from "../../utils/validateEmail";
+import validateZipCode from "../../utils/validateZipCode";
+import axios from "../../utils/requesition/citRequesition"
+import { useNavigate } from 'react-router-dom';
 
 const formsTemplate = {
-	condominium: {
-		name: "",
-		cnpj: "",
-		blocks: "",
-		floors: "",
-	},
-	admin: {
-		name: "",
-		cpf: "",
-		email: "",
-		password: "",
-		confirm: "",
-	},
+    condominium: {
+        name: "",
+        cnpj: "",
+        blocks: "",
+        units: "",
+        floors: "",
+        apartments: "",
+        address: {
+            street: "",
+            number: "",
+            neighborhood: "",
+            city: "",
+            state: "",
+            zipCode: "",
+        },
+    },
+    admin: {
+        name: "",
+        cpf: "",
+		profile: {
+			email: "",
+			password: "",
+			confirm: "",
+		},
+    },
 };
 
-// TODO Adicionar validação de CPF e CNPJ
-// TODO Adicionar validação de email
-// TODO Adicionar validação de senha
-// TODO Adicionar a connexão com a API para enviar os dados
-
-// TODO Não permitir avançar se os campos não estiverem preenchidos
-// TODO Adicionar validação de CPF e CNPJ
-// TODO Adicionar validação de email
-// TODO Adicionar validação de senha
-// TODO Adicionar a connexão com a API para enviar os dados
-
 export default function CondominiumRegistration() {
-	const [data, setData] = React.useState(formsTemplate);
-	const emptyFieldsCondominium = [];
-	const emptyFieldsAdmin = [];
-  
-	// const [emptyFieldsCondominium, setEmptyFieldsCondominium] = React.useState({});
-	// const [emptyFieldsAdmin, setEmptyFieldsAdmin] = React.useState({});
+	const [data, setData] = useState(formsTemplate);
+	const navigate = useNavigate();
 
 	usePageTitle("Cadastro de Condomínio");
 
-	useEffect(() => {
-		console.log(formsTemplate);
-	}, [data]);
+	function register(data) {
+		let admin = {}
+		let condominium = {}
+		console.log('Dados enviados:', data);
+		axios.post("/employee/admin", data.admin).then(response => {
+			admin = response.data;
 
+			data.condominium.managerId = admin.id;
+
+			axios.post("/condominium", data.condominium).then(response => {
+				condominium = response.data;
+
+				navigate(`/login`);
+
+			}).catch(error => {
+				alert('Erro ao enviar os dados:', error);
+				console.error('Erro ao enviar os dados:', error);	
+			});
+
+		}).catch(error => {
+			alert('Erro ao enviar os dados:', error);
+			console.error('Erro ao enviar os dados:', error);	
+		});
+	}
+	
 	function fieldsEmpty(fieldsData) {
-		const fields = Object.keys(fieldsData)
-			.filter((key) => !fieldsData[key])
-			.map((key) => ({ [key]: "Campo obrigatório" }));
-		return fields;
+		const fields = {};
+
+		const checkFields = (data, parentKey = "") => {
+			Object.keys(data).forEach((key) => {
+				const fullKey = parentKey ? `${parentKey}.${key}` : key;
+				if (typeof data[key] === "object" && data[key] !== null) {
+					checkFields(data[key], fullKey);
+				} else if (!data[key]) {
+					fields[fullKey] = "Campo obrigatório";
+				}
+			});
+		};
+
+		checkFields(fieldsData);
+		let fieldsErrors = {};
+		Object.keys(fields).forEach((key) => {
+			if(key.includes('.')) {
+				const keyParts = key.split('.');
+				const parentKey = keyParts[0];
+				if(!fieldsErrors[parentKey]) fieldsErrors[parentKey] = {};
+				fieldsErrors[parentKey][keyParts[1]] = fields[key];
+			}
+			else {
+				fieldsErrors[key] = fields[key];
+			}
+		});
+		return fieldsErrors;
 	}
 	function validateCondominiumForm() {
-		let fieldsErrors;
+		let fieldsErrors = {};
 
-		fieldsErrors += fieldsEmpty(data.condominium);
-		return fieldsErrors.length === 0;
+		if (!validateZipCode(data.condominium.address?.zipCode)) {
+			if (!fieldsErrors.address) fieldsErrors.address = {};
+			fieldsErrors.address['zipCode'] = 'CEP inválido';
+		}
+		
+		if(!validCnpj(data.condominium.cnpj)) fieldsErrors['cnpj'] = 'CNPJ inválido';
+		fieldsErrors = {...fieldsEmpty(data.condominium), ...fieldsErrors};				
+		return fieldsErrors;
 	}
-
+	
 	function validateAdminForm() {
-		let fieldsErrors;
-		fieldsErrors = fieldsEmpty(data.admin);
-		return fieldsErrors.length === 0;
+		let fieldsErrors ={};
+		
+		if(!validCpf(data.admin.cpf)) fieldsErrors['cpf'] = 'CPF inválido';
+		if(!validateEmail(data.admin.profile?.email)) {
+			if (!fieldsErrors.profile) fieldsErrors.profile = {};
+			fieldsErrors.profile['email'] = 'Email inválido'
+		};
+		if(data.admin.profile?.confirm !== data.admin.profile?.password) {
+			if (!fieldsErrors.profile) fieldsErrors.profile = {};
+			fieldsErrors.profile['password'] = 'As senhas não conferem';
+			fieldsErrors.profile['confirm'] = 'As senhas não conferem';
+		};
+		fieldsErrors = {...fieldsEmpty(data.admin), ...fieldsErrors};
+		return fieldsErrors;
 	}
-
+	
 	const updateFieldHandler = (section, field, value) => {
-		setData((prevData) => ({
-			...prevData,
-			[section]: {
-				...prevData[section],
-				[field]: value,
-			},
-		}));
+		setData((prevData) => {
+			const updatedSection = { ...prevData[section] };
+
+			const fieldParts = field.split(".");
+			let currentField = updatedSection;
+
+			for (let i = 0; i < fieldParts.length - 1; i++) {
+				const part = fieldParts[i];
+				if (!currentField[part]) {
+					currentField[part] = {};
+				}
+				currentField = currentField[part];
+			}
+
+			currentField[fieldParts[fieldParts.length - 1]] = value;
+
+			return {
+				...prevData,
+				[section]: updatedSection,
+			};
+		});
 	};
 
 	const steps = [
@@ -79,20 +157,28 @@ export default function CondominiumRegistration() {
 			data={data.condominium}
 			updateFieldHandler={updateFieldHandler}
 			subtitle={data.condominium.name || "Nome do condominio"}
+			fieldsErrors={validateCondominiumForm()}
 		/>,
 		<AdminForm
 			data={data.admin}
 			updateFieldHandler={updateFieldHandler}
 			subtitle={data.condominium.name || "Nome do condominio"}
+			fieldsErrors={validateAdminForm()}
 		/>,
 	];
 
-	return (
-		<Forms.Page
-			steps={steps}
-			imageSource="/condominium-registration.png"
-			validations={[validateCondominiumForm(), validateAdminForm()]}
-			callbak={() => console.log(data)}
-		/>
-	);
+	console.log(validateCondominiumForm());
+	console.log(data);
+
+    return (
+        <Forms.Page
+            steps={steps}
+            imageSource="/condominium-registration.png"
+            validations={[
+                Object.keys(validateCondominiumForm()).length === 0,
+                Object.keys(validateAdminForm()).length === 0,
+            ]}
+            callbak={() => register(data)}
+        />
+    );
 }
