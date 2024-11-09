623063cc9ddac5743b0da3bdec742e8a0339c64c
jaoppb <joaopedroperes06@gmail.com>
2024-11-09 17:48:48 -0300

    add Resident entity and changed from People to Resident in Apartment

---
+++ src/back/src/main/java/com/cit/backend/domain/entity/Resident.java
@@ -0,0 +1,20 @@
+package com.cit.backend.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "residents")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Resident extends People {
+    @OneToOne
+    Apartment apartment;
+}
