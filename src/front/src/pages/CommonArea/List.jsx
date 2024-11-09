0db7215001b0a8f15d0dd43414fa3859d42fecb7
jaoppb <joaopedroperes06@gmail.com>
2024-11-09 12:12:23 -0300

    updated CommonArea list  to match new design

--- src/front/src/pages/CommonArea/List.jsx
+++ src/front/src/pages/CommonArea/List.jsx
@@ -1,27 +1,34 @@
 import React from "react";
 import SearchBar from "../../components/Search/SearchBar";
-import ImageCard from "../../components/ImageCard";
+import PolaroidCard from "../../components/PolaroidCard";
 
 export default function CommonAreaList({ commonAreas, setFocused, role }) {
 	return (
-		<main className="w-full h-full p-4 flex flex-col bg-white">
-			<div className="flex items-center flex-col mb-4">
-				<h1 className="text-center text-4xl mb-2">Áreas Comuns</h1>
-				<div className="bg-zinc-400 h-[1px] w-1/4"></div>
-			</div>
-			<SearchBar role={role} />
+		<main className="w-full h-full flex flex-col items-center bg-gray-low">
+			<div className="flex flex-col px-4 xl:px-0 xl:w-8/12 h-full py-4">
+				<div className="flex items-center flex-col mb-4">
+					<h1 className="text-center text-4xl mb-2">Áreas Comuns</h1>
+					<div className="bg-zinc-400 h-px w-1/4"></div>
+				</div>
+				<SearchBar role={role} />
 
-			<div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 place-content-stretch w-full mt-4 gap-4 max-h-full overflow-auto">
-				{commonAreas.map((commonArea) => (
-					<ImageCard
-						key={commonArea.id}
-						imgSrc={commonArea.img}
-						title={commonArea.name}
-						className="w-full h-full min-h-60"
-						buttonText="Ver Detalhes"
-						buttonAction={() => setFocused(commonArea)}
-					/>
-				))}
+				<div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 place-content-stretch w-full mt-4 gap-4 max-h-full overflow-auto">
+					{commonAreas.map((commonArea) => (
+						<PolaroidCard
+							key={commonArea.id}
+							imgSrc={commonArea.img}
+							title={commonArea.name}
+							className="bg-white"
+						>
+							<button
+								className="text-primary-1000 bg-none"
+								onClick={() => setFocused(commonArea)}
+							>
+								Ver Detalhes
+							</button>
+						</PolaroidCard>
+					))}
+				</div>
 			</div>
 		</main>
 	);
