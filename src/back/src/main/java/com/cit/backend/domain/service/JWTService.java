dd8a313cada6351908c8600f5ed77f25c15cc3e4
jaoppb <joaopedroperes06@gmail.com>
2024-11-09 17:56:29 -0300

    now JWTService throws the errors

--- src/back/src/main/java/com/cit/backend/domain/service/JWTService.java
+++ src/back/src/main/java/com/cit/backend/domain/service/JWTService.java
@@ -4,8 +4,6 @@ package com.cit.backend.domain.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTCreationException;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.cit.backend.api.validator.JWTToken;
 import jakarta.validation.Valid;
@@ -34,33 +32,23 @@ public abstract class JWTService {
             String subject,
             boolean expires
     ) {
-        try {
-            JWTCreator.Builder builder = JWT
-                    .create()
-                    .withIssuer(ISSUER)
-                    .withIssuedAt(new Date())
-                    .withSubject(subject);
-            if (expires) builder.withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION));
-            return builder.sign(getSignKey());
-        } catch (JWTCreationException e) {
-            // TODO criar uma exceção personalizada
-            throw new RuntimeException("Error creating JWTToken", e);
-        }
+        JWTCreator.Builder builder = JWT
+                .create()
+                .withIssuer(ISSUER)
+                .withIssuedAt(new Date())
+                .withSubject(subject);
+        if (expires) builder.withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION));
+        return builder.sign(getSignKey());
     }
 
     protected Boolean isTokenValid(@Valid @JWTToken String token, String subject) {
-        try {
-            return !isTokenExpired(token) && JWT
-                    .require(getSignKey())
-                    .withIssuer(ISSUER)
-                    .build()
-                    .verify(token)
-                    .getSubject()
-                    .equals(subject);
-        } catch (JWTVerificationException e) {
-            // TODO criar uma exceção personalizada
-            throw new RuntimeException("Error validating JWTToken", e);
-        }
+        return !isTokenExpired(token) && JWT
+                .require(getSignKey())
+                .withIssuer(ISSUER)
+                .build()
+                .verify(token)
+                .getSubject()
+                .equals(subject);
     }
 
     private DecodedJWT decode(String token) {
