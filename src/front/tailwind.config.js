c7b530308e1871aafd579906b023607d5c6376b4
jaoppb <joaopedroperes06@gmail.com>
2024-11-10 20:36:00 -0300

    converted tailwind config to ES module

--- src/front/tailwind.config.js
+++ src/front/tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		extend: {
-			colors: {
-				primary: {
-					50: "#f6f8f7",
-					1000: "#42D0B3",
-				},
-				secondary: "#F6F8F7",
+
+export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
+export const theme = {
+	extend: {
+		colors: {
+			primary: {
+				50: "#f6f8f7",
+				1000: "#42D0B3",
 			},
-			fontFamily: {
-				sans: ["Work Sans", "sans-serif"],
-			},
+			secondary: "#F6F8F7",
+		},
+		fontFamily: {
+			sans: ["Work Sans", "sans-serif"],
 		},
 	},
-	plugins: [],
 };
+export const plugins = [];
