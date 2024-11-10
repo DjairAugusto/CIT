4bd62effb1ec13061462ab12b36e4b190e8cee70
jaoppb <joaopedroperes06@gmail.com>
2024-11-10 20:16:38 -0300

    now using Roles utils in SearchBar

--- src/front/src/components/Search/SearchBar.jsx
+++ src/front/src/components/Search/SearchBar.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import SearchInput from "./SearchInput";
 import SearchButton from "./SearchButton";
 import { Search, Filter, Plus } from "lucide-react";
+import Roles from "../../utils/roles";
 
-export default function SearchBar({role}) {
+export default function SearchBar({ useRole = true }) {
 	return (
 		<div className="flex gap-2 w-full">
 			<SearchInput />
 			<SearchButton icon={<Search />} />
-			{role === "ADMIN" && <SearchButton icon={<Plus />} />}
+			{useRole && Roles.isAdmin() && <SearchButton icon={<Plus />} />}
 			<SearchButton icon={<Filter />} />
 		</div>
 	);
