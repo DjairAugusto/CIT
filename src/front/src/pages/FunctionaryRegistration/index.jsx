85f00553a6908fdc7f72ab5e312df86ba7a7d28e
jaoppb <joaopedroperes06@gmail.com>
2024-11-11 13:45:10 -0300

    added temporary missing validations to Forms.Page

--- src/front/src/pages/FunctionaryRegistration/index.jsx
+++ src/front/src/pages/FunctionaryRegistration/index.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import EmployeeForm from "./EmployeeForm";
 import VehicleForm from "./VehicleForm";
 import { Forms } from "../../components/Forms";
 
 // TODO integração do registro de funcionário com o backs
+// TODO fazer as validações do Forms.Page
 export default function FunctionaryRegistration() {
 	const steps = [<EmployeeForm />, <VehicleForm />];
+	const validations = useState([]);
 
 	return (
 		<Forms.Page
 			steps={steps}
+			validations={validations}
 			imageSource="/funcionario-cadastro.jpg"
 		/>
 	);
