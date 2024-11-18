b2716beb19533ef5c857daf7fd40548db0fe910b
jaoppb <joaopedroperes06@gmail.com>
2024-11-18 17:04:13 -0300

    passing rest value to select in FormsSelect typo

--- src/front/src/components/Forms/FormsSelect.jsx
+++ src/front/src/components/Forms/FormsSelect.jsx
@@ -1,18 +1,22 @@
-export default function FormsSelect({ className, options, required = false, ...rest }) {
+export default function FormsSelect({
+	className,
+	options,
+	required = false,
+	...rest
+}) {
 	return (
 		<div className="flex items-center px-4 py-2 border-sm border-medium bg-zinc-50 border-b-2 border-transparent rounded-md">
 			{required && <span className="text-red-500">*</span>}
-			<select required={required} className="relative w-full bg-transparent h-12 outline-none text-dark placeholder:text-dark text-zinc-700 placeholder:text-zinc-700 peer">
-				{
-					options.map(option => (
-						<option
-							key={option.value}
-							value={option.value}
-						>
-							{option.text}
-						</option>
-					))
-				}
+			<select
+				required={required}
+				className="relative w-full bg-transparent h-12 outline-none text-dark placeholder:text-dark text-zinc-700 placeholder:text-zinc-700 peer"
+				{...rest}
+			>
+				{options.map((option) => (
+					<option key={option.value} value={option.value}>
+						{option.text}
+					</option>
+				))}
 			</select>
 		</div>
 	);
