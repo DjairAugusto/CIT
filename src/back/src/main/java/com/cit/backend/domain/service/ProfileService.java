5c58fb5f44cf0a25333d1b828585212f7ae1a1af
jaoppb <joaopedroperes06@gmail.com>
2024-11-20 18:58:24 -0300

    automatically encoding password when saving in ProfileService

--- src/back/src/main/java/com/cit/backend/domain/service/ProfileService.java
+++ src/back/src/main/java/com/cit/backend/domain/service/ProfileService.java
@@ -4,14 +4,22 @@ import com.cit.backend.domain.entity.Profile;
 import com.cit.backend.domain.repository.ProfileRepository;
 import com.cit.backend.exceptions.UniqueColumnAlreadyExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class ProfileService implements UserDetailsService {
     @Autowired
     private ProfileRepository profileRepository;
+
+    @Autowired
+    @Lazy
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) {
@@ -23,6 +31,7 @@ public class ProfileService implements UserDetailsService {
             throw new UniqueColumnAlreadyExistsException("Email has already been registered");
         }
 
+        profile.setPassword(passwordEncoder.encode(profile.getPassword()));
         return profileRepository.save(profile);
     }
 
