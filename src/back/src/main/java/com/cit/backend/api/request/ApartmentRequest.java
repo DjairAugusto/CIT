3dfdf94929d52ecf221761564415eb6fb0476856
jaoppb <joaopedroperes06@gmail.com>
2024-11-20 19:03:14 -0300

    removed unused fields from ApartmentRequest

--- src/back/src/main/java/com/cit/backend/api/request/ApartmentRequest.java
+++ src/back/src/main/java/com/cit/backend/api/request/ApartmentRequest.java
@@ -1,9 +1,6 @@
 package com.cit.backend.api.request;
 
-import com.cit.backend.api.validator.JWTToken;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,21 +11,7 @@ import java.util.Set;
 @Getter
 @AllArgsConstructor
 public class ApartmentRequest {
-    private Long id;
-
-    @NotNull(message = "Number is mandatory")
-    @PositiveOrZero(message = "Number must be equals or greater than zero")
-    private int number;
-
-    @NotBlank(message = "Token is mandatory")
-    @JWTToken()
-    private String token;
-
-    @NotNull(message = "Unit id is mandatory")
-    @PositiveOrZero(message = "Unit id must be equals or greater than zero")
-    private Long unitId;
-
+    @Valid
     private Set<ResidentRequest> residents;
     private Set<VehicleRequest> vehicles;
-    private Set<ProfileRequest> profiles;
 }
