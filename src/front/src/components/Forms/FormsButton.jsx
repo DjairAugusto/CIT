91cf2e8dd60c1c53df3b4c2aceec6c4de016e5cf
jaoppb <joaopedroperes06@gmail.com>
2024-11-23 20:20:26 -0300

    added opacity to Forms.Button when disabled

--- src/front/src/components/Forms/FormsButton.jsx
+++ src/front/src/components/Forms/FormsButton.jsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function FormsButon({ children, className, ...rest }) {
 	return (
 		<button
-			className={"bg-primary-1000 text-white h-10 mt-10 " + className}
+			className={
+				"bg-primary-1000 text-white h-10 mt-10 disabled:opacity-75 transition-opacity duration-300 " +
+				className
+			}
 			type="submit"
 			{...rest}
 		>
