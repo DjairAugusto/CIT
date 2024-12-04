176fcec94b60789a13a59c01ad4557069c5a039a
jaoppb <joaopedroperes06@gmail.com>
2024-12-04 00:36:02 -0300

    added missing NoArgsConstructor to some responses and requests

--- src/back/src/main/java/com/cit/backend/api/request/ContactRequest.java
+++ src/back/src/main/java/com/cit/backend/api/request/ContactRequest.java
@@ -4,10 +4,12 @@ import com.cit.backend.api.validator.Phone;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class ContactRequest {
     @Phone(message = "Invalid phone")
