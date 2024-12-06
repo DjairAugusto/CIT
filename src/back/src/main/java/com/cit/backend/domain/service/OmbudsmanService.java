54ae2e3e8ee7ea86ed73221b362781ee247b4647
jaoppb <joaopedroperes06@gmail.com>
2024-12-06 01:20:00 -0300

    changes to OmbudsmanService
    added findAllByApartment
    added findAllByCondominium

--- src/back/src/main/java/com/cit/backend/domain/service/OmbudsmanService.java
+++ src/back/src/main/java/com/cit/backend/domain/service/OmbudsmanService.java
@@ -1,10 +1,16 @@
 package com.cit.backend.domain.service;
 
+import com.cit.backend.domain.entity.Apartment;
+import com.cit.backend.domain.entity.Condominium;
 import com.cit.backend.domain.entity.Ticket;
 import com.cit.backend.domain.repository.OmbudsmanRepository;
 import com.cit.backend.exceptions.UniqueColumnAlreadyExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class OmbudsmanService {
@@ -16,7 +22,7 @@ public class OmbudsmanService {
     }
 
     public Ticket create(Ticket ticket) {
-        if (ombudsmanRepository.findById(ticket.getId()).isPresent()) {
+        if (ticket.getId() != null && ombudsmanRepository.findById(ticket.getId()).isPresent()) {
             throw new UniqueColumnAlreadyExistsException("Ticket has already been registered");
         }
 
@@ -25,5 +31,19 @@ public class OmbudsmanService {
 
     public Ticket update(Ticket ticket) {
         return ombudsmanRepository.save(ticket);
+    }
+
+    public List<Ticket> findAllByApartment(Apartment apartment) {
+        return ombudsmanRepository.findAllByApartment(apartment);
+    }
+
+    public List<Ticket> findAllByCondominium(Condominium condominium) {
+        Set<Apartment> apartments = new HashSet<>();
+        condominium.getBlockList().forEach(block -> {
+            block.getUnits().forEach(unit -> {
+                apartments.addAll(unit.getApartments());
+            });
+        });
+        return ombudsmanRepository.findAllByApartmentIn(apartments);
     }
 }
