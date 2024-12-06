278f507039b5186ba5cfde7eaa1fe1493a6609bf
jaoppb <joaopedroperes06@gmail.com>
2024-12-06 13:15:39 -0300

    added value and onChange props to SearchBar

--- src/front/src/components/Search/SearchBar.jsx
+++ src/front/src/components/Search/SearchBar.jsx
@@ -4,10 +4,10 @@ import SearchButton from "./SearchButton";
 import { Search, Filter, Plus } from "lucide-react";
 import Roles from "../../utils/roles";
 
-export default function SearchBar({ useRole = true }) {
+export default function SearchBar({ value, onChange, useRole = true }) {
 	return (
 		<div className="flex gap-2 w-full">
-			<SearchInput />
+			<SearchInput value={value} onChange={onChange} />
 			<SearchButton icon={<Search />} />
 			{useRole && Roles.isAdmin() && <SearchButton icon={<Plus />} />}
 			<SearchButton icon={<Filter />} />
--- src/front/src/components/Search/SearchInput.jsx
+++ src/front/src/components/Search/SearchInput.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-export default function SearchInput({ placeholder }) {
+export default function SearchInput({ value, onChange, placeholder }) {
 	return (
 		<input
 			className="bg-gray-300 rounded-2xl px-3 text-lg flex-1 min-w-0"
 			type="text"
 			placeholder={placeholder}
+			value={value}
+			onChange={onChange}
 		/>
 	);
 }
