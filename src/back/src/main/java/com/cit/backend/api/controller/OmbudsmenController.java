f9c087a9007990a14264d9ed871b17282dfdd7c8
jaoppb <joaopedroperes06@gmail.com>
2024-12-06 01:21:09 -0300

    changes to OmbudsmenController
    added getAll based on logged profile
    fixed create method

--- src/back/src/main/java/com/cit/backend/api/controller/OmbudsmenController.java
+++ src/back/src/main/java/com/cit/backend/api/controller/OmbudsmenController.java
@@ -3,11 +3,18 @@ package com.cit.backend.api.controller;
 import com.cit.backend.api.mapper.OmbudsmanMapper;
 import com.cit.backend.api.request.OmbudsmenRequest;
 import com.cit.backend.api.response.OmbudsmenResponse;
-import com.cit.backend.domain.entity.Ticket;
+import com.cit.backend.domain.entity.*;
+import com.cit.backend.domain.entity.enums.StatusTicket;
+import com.cit.backend.domain.service.EmployeeService;
 import com.cit.backend.domain.service.OmbudsmanService;
+import com.cit.backend.domain.service.ResidentService;
+import com.cit.backend.exceptions.EmployeeNotRegisteredInACondominiumException;
+import com.cit.backend.exceptions.InvalidResidentException;
 import com.cit.backend.exceptions.MissingVariableException;
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +28,36 @@ public class OmbudsmenController {
     @Autowired
     private OmbudsmanService ombudsmanService;
 
+    @Autowired
+    private ResidentService residentService;
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping
+    public ResponseEntity<List<OmbudsmenResponse>> getAll() {
+        Profile profile = (Profile) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Employee employee = employeeService.findByProfile(profile);
+        Resident resident = residentService.getFromProfile(profile);
+
+        List<Ticket> tickets;
+        if (employee != null) {
+            Condominium condominium = employee.getCondominium();
+            if (condominium == null)
+                throw new EmployeeNotRegisteredInACondominiumException("Employee is not registered in a condominium");
+
+            tickets = ombudsmanService.findAllByCondominium(condominium);
+        } else if (resident != null) {
+            Apartment apartment = resident.getApartment();
+            if (apartment == null) throw new InvalidResidentException("Resident is not registered in an apartment");
+
+            tickets = ombudsmanService.findAllByApartment(apartment);
+        } else throw new RuntimeException("User is not an employee or a resident");
+
+        List<OmbudsmenResponse> response = ombudsmanMapper.toOmbudsmenResponse(tickets);
+        return ResponseEntity.ok(response);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<OmbudsmenResponse> findById(@PathVariable Long id) {
         Ticket ticket = ombudsmanService.findById(id);
@@ -29,8 +66,17 @@ public class OmbudsmenController {
     }
 
     @PostMapping
+    @RolesAllowed("RESIDENT")
     public ResponseEntity<OmbudsmenResponse> create(@RequestBody OmbudsmenRequest request) {
+        Profile profile = (Profile) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Resident resident = residentService.getFromProfile(profile);
+        Apartment apartment = resident.getApartment();
+
+        if (apartment == null) throw new InvalidResidentException("Resident is not registered in an apartment");
+
         Ticket ticket = ombudsmanMapper.toTicket(request);
+        ticket.setApartment(apartment);
+        ticket.setStatus(StatusTicket.OPEN);
         Ticket saved = ombudsmanService.create(ticket);
         OmbudsmenResponse response = ombudsmanMapper.toOmbudsmenResponse(saved);
         return ResponseEntity.ok(response);
